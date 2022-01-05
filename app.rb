773486221b2a3b04d9f4044a1d9c1b022e17821c
luis leon <leonluis380@yahoo.com>
2022-01-04 21:28:53 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,10 +1,21 @@
+
 require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    get "/" do 
-        erb :team
-    end
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @c = params[:c]
+    @d = params[:d]
+    @ef = params[:e]
+    @f = params[:f]
+    @g = params[:g]
+    erb :team
+  end
 
 end
